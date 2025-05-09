53d5721047614c90fbf42bdebc2aabeef9ce5bc8
mdenitti <massimodn77@gmail.com>
2025-05-09 13:12:26 +0200

    Add initial project structure with routes, storage, tests, and Vite configuration
    - Created console route for inspiring quotes.
    - Added web route for the welcome view.
    - Set up .gitignore files for various storage directories.
    - Implemented basic feature and unit tests.
    - Configured Vite for asset management with Laravel and Tailwind CSS.

---
+++ app/Models/Patient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Treatment;
+
+class Patient extends Model
+{
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Owner::class);
+    }
+    public function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
+}
